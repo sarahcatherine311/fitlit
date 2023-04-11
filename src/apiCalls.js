53b3f3f7bd8972bdce6acff95120e93eff52c8d3
“Sarah <sarahcatherine311@gmail.com>
2023-04-11 16:06:56 -0600

    feat: Update the apiCalls.js file to include the POST

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,10 +4,19 @@ const userDataFetch = (data) => {
     .catch(err => console.log(`Error at: ${err}`))
 }
 
-// const userPost = () => {
-//   return fetch()
-// }
+const userPost = (data) => {
+  return fetch('http://localhost:3001/api/v1/hydration', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(data => data.json())
+  .then(json => console.log(json))
+  .catch(err => console.log(`Error at: ${err}`))
+}
 
-export { userDataFetch };
-
+export { userDataFetch }
+export {userPost}
 
