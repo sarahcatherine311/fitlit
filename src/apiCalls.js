5c4917c522997caf8ecbde3bed5f9f61113a2257
“Sarah <sarahcatherine311@gmail.com>
2023-04-11 16:01:53 -0600

    feat: Update the GET to pull data from the local server

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,8 +1,12 @@
 const userDataFetch = (data) => {
-  return fetch(`https://fitlit-api.herokuapp.com/api/v1/${data}`)
+  return fetch(`http://localhost:3001/api/v1/${data}`)
     .then(data => data.json())
     .catch(err => console.log(`Error at: ${err}`))
 }
+
+// const userPost = () => {
+//   return fetch()
+// }
 
 export { userDataFetch };
 
